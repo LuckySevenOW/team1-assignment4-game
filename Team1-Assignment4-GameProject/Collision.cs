2019500812e6027b462008f0caa6ade1508cdba8
danco <daniel.coombs@mohawkcollege.ca>
2024-11-29 11:12:11 -0500

    add variables to Shape_Creation and Collision

--- Team1-Assignment4-GameProject/Collision.cs
+++ Team1-Assignment4-GameProject/Collision.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Game10003;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,5 +11,12 @@ namespace Team1_Assignment4_GameProject
     public class Collision
     {
         public Shape_Creation shape_Creation;
+
+        public Vector2 barrelLocation = new Vector2(Window.Width - 50, 100);
+        public int barrelSize = 15;
+
+        public Vector2 barrelLastPosition;
+        public float barrelDirectionX = 3;
+        public float barrelDirectionY = -3;
     }
 }
--- Team1-Assignment4-GameProject/Shape_Creation.cs
+++ Team1-Assignment4-GameProject/Shape_Creation.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,5 +10,16 @@ namespace Team1_Assignment4_GameProject
     public class Shape_Creation
     {
         public Collision collision;
+
+        public Vector2 playerPosition = new Vector2(250, 600);
+        public Vector2 playerSize = new Vector2(60, 25);
+        public Vector2 playerLastPosition;
+
+        public float playerLeftEdge;
+        public float playerRightEdge;
+        public float playerTopEdge;
+        public float playerBottomEdge;
+
+        public float platformTopEdge;
     }
 }
