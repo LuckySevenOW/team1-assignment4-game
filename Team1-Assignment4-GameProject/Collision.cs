0e2736bb0197d91341ada9e389831db45b7dd491
danco <daniel.coombs@mohawkcollege.ca>
2024-11-29 10:59:18 -0500

    Add classes, connect classes

---
+++ Team1-Assignment4-GameProject/Collision.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team1_Assignment4_GameProject
+{
+    public class Collision
+    {
+        public Shape_Creation shape_Creation;
+    }
+}
--- Team1-Assignment4-GameProject/Game.cs
+++ Team1-Assignment4-GameProject/Game.cs
@@ -1,6 +1,8 @@
 ﻿// Include code libraries you need below (use the namespace).
 using System;
 using System.Numerics;
+using System.Security.Principal;
+using Team1_Assignment4_GameProject;
 
 // The namespace your code is in.
 namespace Game10003
@@ -11,14 +13,20 @@ namespace Game10003
     public class Game
     {
         // Place your variables here:
-
+        Shape_Creation shape_Creation = new Shape_Creation();
+        Collision collision = new Collision();
 
         /// <summary>
         ///     Setup runs once before the game loop begins.
         /// </summary>
         public void Setup()
         {
+            Window.SetTitle("W.I.P");
 
+            Window.SetSize(600, 1000);
+
+            collision.shape_Creation = shape_Creation;
+            shape_Creation.collision = collision;
         }
 
         /// <summary>
@@ -26,7 +34,7 @@ namespace Game10003
         /// </summary>
         public void Update()
         {
-
+             
         }
     }
 }
